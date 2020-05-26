14ca54a8aa9e8a7a99f47bdbbf07aab0a6b7d7c9
Dawid Jaloza <dawidjaloza@gmail.com>
2020-05-26 19:21:57 +0200

    Before BufferedWriter commit

--- src/com/example/Locations.java
+++ src/com/example/Locations.java
@@ -19,28 +19,11 @@ public class Locations implements Map<Integer, Location> {
                 }
             }
         }
-
-
-//        FileWriter locFile = null;
-//        try {
-//            locFile = new FileWriter("locations.txt");
-//            for (Location location : locations.values()) {
-//                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
-//            }
-//        } finally { // executed every time doesn't matter was caught or not
-//            System.out.println("In finally block");
-//            if (locFile != null) {
-//                System.out.println("Attempting to close locFile");
-//                locFile.close();
-//            }
-//        }
     }
 
     //will be executed once only when Locations class loaded
     static {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new FileReader("locations.txt"));
+        try (Scanner scanner = new Scanner(new FileReader("locations_big.txt"))) {
             scanner.useDelimiter(",");
             while(scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
@@ -52,24 +35,12 @@ public class Locations implements Map<Integer, Location> {
             }
         } catch(IOException e) {
             e.printStackTrace();
-        } finally {
-            if(scanner != null) {
-                scanner.close();
-            }
         }
 
 //        Now read the exits
-        try {
-            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
-            scanner.useDelimiter(",");
-            while(scanner.hasNextLine()) {
-//                int loc = scanner.nextInt();
-//                scanner.skip(scanner.delimiter());
-//                String direction = scanner.next();
-//                scanner.skip(scanner.delimiter());
-//                String dest = scanner.nextLine();
-//                int destination = Integer.parseInt(dest);
-                String input = scanner.nextLine();
+        try (BufferedReader dirFile = new BufferedReader(new FileReader("directions_big.txt"))) {
+            String input;
+            while((input = dirFile.readLine()) != null) {
                 String[] data = input.split(",");
                 int loc = Integer.parseInt(data[0]);
                 String direction = data[1];
@@ -81,42 +52,7 @@ public class Locations implements Map<Integer, Location> {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if(scanner != null) {
-                scanner.close();
-            }
         }
-
-
-
-//        Map<String, Integer> tempExit = new HashMap<>();
-//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("W", 2);
-//        tempExit.put("E", 3);
-//        tempExit.put("S", 4);
-//        tempExit.put("N", 5);
-//        locations.put(1, new Location(1, "You are standing at the end of a read before a small brick building",tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("N", 5);
-//        locations.put(2, new Location(2, "You are at top of a hill", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("W", 1);
-//        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("N", 1);
-//        tempExit.put("W", 2);
-//        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
-//
-//        tempExit = new HashMap<>();
-//        tempExit.put("S", 1);
-//        tempExit.put("W", 2);
-//        locations.put(5, new Location(5, "You are in the forest", tempExit));
-        //
     }
 
     @Override
